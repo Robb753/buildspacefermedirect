2553ed4e22f6d3a6537e89b2aebb39c737a768d1
Robb753 <122726102+Robb753@users.noreply.github.com>
2024-07-14 21:08:05 +0200

    maj

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,14 @@ app.use(express.json())
 app.use(cors(corsOptions));
 app.use(bodyParser.json())
 
+// Middleware pour ajouter les en-têtes CORS manuellement
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://fermedirect.netlify.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 // Connect to MongoDB
 connectToServer()
   .then(() => {
