389bffd5b8d88302e36ffd58924ca4cfed2894a9
Robb753 <122726102+Robb753@users.noreply.github.com>
2024-07-14 19:33:25 +0200

    maj

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('subscribe'))
 
 // Connect to MongoDB
 connectToServer()
@@ -29,7 +30,7 @@ connectToServer()
       }
     });
 
-    app.post("/subscribe", async (req, res) => {
+    app.post("/api/subscribe", async (req, res) => {
       const { email } = req.body;
       console.log(`Received email: ${email}`);
       try {
