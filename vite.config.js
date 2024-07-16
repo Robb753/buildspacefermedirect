94b7aac9ef32c92a2cd25b79e4a87ec1be4eaa85
Robb753 <122726102+Robb753@users.noreply.github.com>
2024-07-16 07:51:43 +0200

    maj

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-require("dotenv").config();
+import dotenv from "dotenv";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     target: "esnext",
     outDir: "dist",
   },
-  plugins: [react()],
   define: {
-    "process.env.VITE_API_URL_USERS": JSON.stringify(
-      process.env.VITE_API_URL_USERS
-    ),
     "process.env.VITE_API_URL_SUBSCRIBE": JSON.stringify(
       process.env.VITE_API_URL_SUBSCRIBE
+    ),
+    "process.env.VITE_API_URL_USERS": JSON.stringify(
+      process.env.VITE_API_URL_USERS
     ),
     "process.env.VITE_GOOGLE_MAPS_API_KEY": JSON.stringify(
       process.env.VITE_GOOGLE_MAPS_API_KEY
